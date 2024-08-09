8e00226556701065448c874fec5efd499604346f
ifurkanturkoglu <turkoglu.ifurkan@gmail.com>
2024-08-09 15:59:53 +0300

    initial commit

---
+++ AIProject/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace AIProject;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ AIProject/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace AIProject;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
